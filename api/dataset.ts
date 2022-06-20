b8b93f79eaabf65154eefa97a198b87bba8a31ff
Chicheng Liu <liuchicheng0401@hotmail.com>
2022-06-21 00:25:26 +0200

    feat: read multiple files into one response

--- api/dataset.ts
+++ api/dataset.ts
@@ -2,18 +2,39 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 import * as fs from "fs";
 import { parse } from "csv-parse";
 
+const readFile : (file : string) => Promise<string[][]> = (file : string) => {
+    return new Promise((resolve, reject) => {
+        const rows : string[][] = [];
+        fs.createReadStream("static/" + file)
+            .pipe(parse({ delimiter: ",", from_line: 1 }))
+            .on("data", (row : string[]) => {
+                rows.push(row);
+            })
+            .on("end", () => {
+                resolve(rows);
+            })
+            .on("error", (error) => {
+                console.log(error);
+            });
+    })
+}
 
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
-    let result : number[][] = [];
-    fs.createReadStream("static/train_x.csv")
-        .pipe(parse({ delimiter: ",", from_line: 1 }))
-        .on("data", (row: string[]) => {
-            result.push(row.map(element => parseFloat(element)).slice());
-        })
-        .on("end", () => {
-            res.status(200).json(result);
-        })
-        .on("error", (error) => {
+    const promises : Promise<string[][]>[] = [];
+    fs.readdir('static', (error, files) => {
+        console.log(files);
+        for (let i = 0; i < files.length; i++) {
+            promises.push(readFile(files[i]));
+        }
+        Promise.all(promises).then((data) => {
+            const trainingSet = data[0].map(row => row.map(element => parseFloat(element)));
+            const labelSet = data[1].map(row => parseInt(row[0]));
+            res.status(200).json({
+                trainingSet,
+                labelSet
+            });
+        }).catch(error => {
             console.log(error);
         })
+    })
 }
